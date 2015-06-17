6f6b34580801d0aae1433f04f9159c75ce7d9d17
Naveen Kumar Dungarwal <er.naveen1911@gmail.com>
2015-06-17 17:09:54 +0530

    Parse demo code uplaod

---
+++ ParseDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ParseDemo
+//
+//  Created by Naveen Kumar Dungarwal on 6/17/15.
+//  Copyright (c) 2015 Naveen Kumar Dungarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
