fb3f0170bbb18795f252f7f11b844650566781df
Naveen Kumar Dungarwal <er.naveen1911@gmail.com>
2015-06-24 11:59:53 +0530

    added few more code

--- ParseDemo/AppDelegate.m
+++ ParseDemo/AppDelegate.m
@@ -31,6 +31,20 @@
                                                                              categories:nil];
     [application registerUserNotificationSettings:settings];
     [application registerForRemoteNotifications];
+    
+    [[[UIAlertView alloc]initWithTitle:@"launchOptions" message:[NSString stringWithFormat:@"userInfo :%@",launchOptions] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil] show];
+    
+    
+    UILocalNotification *notification = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
+    if (notification) {
+        NSLog(@"AppDelegate didFinishLaunchingWithOptions");
+        [[[UIAlertView alloc]initWithTitle:@"launchOptions" message:[NSString stringWithFormat:@"userInfo :%@",notification.userInfo] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil] show];
+        [application setApplicationIconBadgeNumber:0];
+//        [application cancelAllLocalNotifications];
+        
+        
+    }
+
 
     
     
@@ -64,6 +78,31 @@
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     
     [PFPush handlePush:userInfo];
+    [[[UIAlertView alloc]initWithTitle:@"ReceiveRemoteNotification" message:[NSString stringWithFormat:@"userInfo :%@",userInfo] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil] show];
+    
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler {
+    
+    NSLog(@"didReceiveRemoteNotification fetchCompletionHandler **********************");
+    NSLog(@"userInfo background: %@", userInfo);
+    [[NSUserDefaults standardUserDefaults]setBool:TRUE forKey:@"IS_NOTIFICATION"];\
+     [[[UIAlertView alloc]initWithTitle:@"UIBackgroundFetchResult" message:[NSString stringWithFormat:@"userInfo :%@",userInfo] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil] show];
+    //    [CommonMethods showAlertWithTitle:@"LUK" message:@"New Video Reciceved from LUK"];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    NSInteger count = [UIApplication sharedApplication].applicationIconBadgeNumber;
+    if (count>0) {
+        [[[UIAlertView alloc]initWithTitle:@"applicationWillEnterForeground" message:[NSString stringWithFormat:@"userInfo :%d",count] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil] show];
+        [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+        [[UIApplication sharedApplication] cancelAllLocalNotifications];
+        [[NSUserDefaults standardUserDefaults]setBool:TRUE forKey:@"IS_NOTIFICATION"];
+        
+
+            //        [CommonMethods showAlertWithTitle:@"LUK" message:@"New Video Reciceved from LUK"];
+    }
     
 }
 
@@ -79,11 +118,10 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+//    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    [[[UIAlertView alloc]initWithTitle:@"didbecomeactive" message:@"" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil] show];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
